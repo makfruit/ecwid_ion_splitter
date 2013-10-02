ae5439ba0a85104853281844ecd30db1e6cb9175
Matvey Kuritsyn <matvey.kuritsyn@gmail.com>
2013-10-03 00:43:24 +0400

    Splitter script

---
+++ ecwid_ion_splitter.php
@@ -0,0 +1,34 @@
+<?
+/*
+ * HTTP requests splitter for Ecwid ION calls
+ *
+ * https://github.com/makfruit/ecwid_ion_splitter
+ * Ecwid: http://www.ecwid.com
+ * Ecwid ION docs: http://kb.ecwid.com/w/page/43696516/Instant%20Order%20Notifications
+ */
+
+
+// Endpoint URLs
+$endpoints = array(
+  'http://google.com',
+  'http://yahoo.com'
+);
+
+// Init and configure CURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+
+foreach ($endpoints as $url) {
+  // Set request URL
+  curl_setopt($ch, CURLOPT_URL, $url);
+  
+  // Make an HTTP request
+  curl_exec($ch);
+}
+
+// CLose connection
+curl_close($ch);
+
+?>
